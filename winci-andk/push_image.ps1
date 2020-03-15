ff202ee29c0e39aa710ba811d795d7038ed71106
Mizar <mizar.jp@gmail.com>
2020-03-15 13:59:25 +0900

    wip20200315

---
+++ winci-andk/push_image.ps1
@@ -0,0 +1,10 @@
+﻿Set-Location $PSScriptRoot;
+
+$Tags = @('latest';1909;'ltsc2019';);
+$Targets = @('winci-andk';);
+
+$Targets|ForEach-Object { $Target = $_;
+  $Tags|ForEach-Object { $Tag = $_;
+    docker image push mizarjp/${Target}:${Tag};
+  }
+}
