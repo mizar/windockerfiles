8a35702d8f03c7cdca38b3ebd4c62227967e1160
Mizar <mizar.jp@gmail.com>
2020-06-08 09:01:04 +0900

    wip20200608

--- winci-msys2/push_image.ps1
+++ winci-msys2/push_image.ps1
@@ -2,11 +2,12 @@
 
 #& $Env:ProgramFiles\\Docker\\Docker\\DockerCli.exe -SwitchWindowsEngine
 
+$DockerUser = 'mizarjp';
 $Tags = @('latest';'2004';'1909';'ltsc2019';);
 $Targets = @('winci-msys2';'winci-msys2-base';);
 
 $Targets|ForEach-Object { $Target = $_;
     $Tags|ForEach-Object { $Tag = $_;
-        docker image push mizarjp/${Target}:${Tag};
+        docker image push ${DockerUser}/${Target}:${Tag};
     }
 }
